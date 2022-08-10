125a65616e36c92e6d72e8f3b6ff3d4e1d122e0f
Joel Lim <joel.lim@joels-mbp.lan>
2022-08-10 21:52:39 +0800

    cfr: store user passwords with hash

--- pages/api/auth/users.ts
+++ pages/api/auth/users.ts
@@ -1,7 +1,14 @@
 import { Collection, ObjectId, WithId } from "mongodb";
 import { connectToDatabase } from "../external/mongodb";
 
+import bcrypt from "bcrypt";
+
 export type CreateUserRequest = {
+    email: string;
+    password: string;
+};
+
+type DbUser = {
     email: string;
     password: string;
 };
@@ -10,16 +17,19 @@ export type User = {
     email: string;
 };
 
-let collection: Collection<User> | null;
+let collection: Collection<DbUser> | null;
 const { db } = await connectToDatabase();
 try {
-    await db.createCollection<User>("users");
+    await db.createCollection<DbUser>("users");
 } catch (e) {
     console.log("collection 'users' probably exists");
 }
-collection = await db.collection<User>("users");
+collection = await db.collection<DbUser>("users");
+
+const saltRounds = await bcrypt.genSalt();
 
 export async function createUser(userRequest: CreateUserRequest): Promise<{ id: ObjectId } | null> {
+    userRequest.password = await bcrypt.hash(userRequest.password, saltRounds);
     const response = await collection!.insertOne(userRequest);
     return { id: response.insertedId };
 }
@@ -28,5 +38,15 @@ export async function findUserByEmailAndPassword(
     email: string,
     password: string,
 ): Promise<WithId<User> | null> {
-    return await collection!.findOne({ email, password }, { projection: { email: 1 } });
+    const user = await collection!.findOne({ email });
+
+    if (!user) {
+        return null;
+    }
+
+    if (!(await bcrypt.compare(password, user.password))) {
+        return null;
+    }
+
+    return user as WithId<User>;
 }
