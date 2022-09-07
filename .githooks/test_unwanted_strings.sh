bf2d751caac64df1848c484967c53b3fba953409
Joel Lim <joel.lim@joels-mbp.lan>
2022-09-07 18:46:50 +0800

    ci/cd: clean test unwanted strings

--- .githooks/test_unwanted_strings.sh
+++ .githooks/test_unwanted_strings.sh
@@ -1,12 +1,15 @@
 #/bin/sh
 
 found=$(grep -R --exclude-dir=node_modules --exclude-dir=.next --exclude-dir=.githooks --exclude-dir=.git 'console.log')
-count=$(echo "$found" | grep -e '^[^#]' | wc -l)
+found=$(echo "$found" | grep -v -E ./log.ts )
+count=$(echo "$found" | grep -e '^[^#]' | grep -v -E ./log.ts | wc -l)
 echo console.log found: "$found"
 echo count: "$count"
 
+exit
+
 if [ $count -ne 1 ]; then
-    echo remove "$count" console.log "statement(s)"
+    echo remove "$count" - 1 console.log "statement(s)"
     echo $(echo "$found" | tail --lines=+1)
     exit 1
 fi
