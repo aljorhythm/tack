ef8059f068c681b14f83f4d267fb8aec0be68a48
Joel Lim <joel.lim@joels-mbp.lan>
2022-09-12 19:09:57 +0800

    ui: refactor navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,14 +7,23 @@ import { useEffect, useState } from "react";
 import { type UserType } from "../pages/api/user/types";
 import * as api from "../pages/api/client";
 import { FaSearch, FaPlus } from "react-icons/fa";
+import { IconType } from "react-icons";
 
 enum NavbarMode {
-    Search,
-    Add,
+    Search = "search",
+    Add = "add",
 }
+
+type NavbarModeDetails = {
+    className: string;
+    setModeIcon: IconType;
+    btnOnClick: Function;
+    btnText: string;
+    inputPlaceholder: string;
+};
+
 function NavbarInput({ username }: { username: string }) {
     const [mode, setMode] = useState<NavbarMode>(NavbarMode.Add);
-    const [placeholder, setPlaceholder] = useState("");
     const [inputTextValue, setInputTextValue] = useState("");
     const router = useRouter();
 
@@ -29,53 +38,101 @@ function NavbarInput({ username }: { username: string }) {
         router.push({ pathname: `/profile/${username}`, query: { query: inputTextValue } });
     }
 
-    useEffect(() => {
-        const map: { [key in NavbarMode]: string } = {
-            [NavbarMode.Add]: "Add a tack https://...",
-            [NavbarMode.Search]: "Search",
-        };
-        setPlaceholder(map[mode]);
-    }, [mode]);
+    const modeDetails: { [key in NavbarMode]: NavbarModeDetails } = {
+        [NavbarMode.Add]: {
+            setModeIcon: FaPlus,
+            className: "add-mode",
+            btnOnClick: addTack,
+            btnText: "add",
+            inputPlaceholder: "Add a tack https://...",
+        },
+        [NavbarMode.Search]: {
+            setModeIcon: FaSearch,
+            className: "search-mode",
+            btnOnClick: search,
+            btnText: "search",
+            inputPlaceholder: "Search",
+        },
+    };
 
     return (
-        <div className="flex space-x-2">
-            <div className="flex space-x-2 hover:cursor-pointer">
-                {mode != NavbarMode.Search ? (
-                    <FaSearch className="search-mode" onClick={() => setMode(NavbarMode.Search)} />
-                ) : (
-                    <></>
-                )}
-                {mode != NavbarMode.Add ? (
-                    <FaPlus className="add-mode" onClick={() => setMode(NavbarMode.Add)} />
-                ) : (
-                    <></>
-                )}
-            </div>
+        <div className="flex flex-wrap space-x-2 items-center hover:cursor-pointer">
+            {Object.entries(modeDetails).map(([modeKey, details]) => {
+                if (mode == modeKey) {
+                    return <></>;
+                }
+                const Icon = details.setModeIcon;
+                return (
+                    <Icon
+                        key={modeKey}
+                        className={details.className}
+                        onClick={() => setMode(modeKey as NavbarMode)}
+                    />
+                );
+            })}
+
             <input
                 className="bg-slate-50 lg:w-96 border border-slate-300 text-slate-900 text-sm rounded focus:ring-slate-500 focus:border-slate-500 block p-2.5 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-slate-500 dark:focus:border-slate-500"
-                placeholder={placeholder}
+                placeholder={modeDetails[mode].inputPlaceholder}
                 onChange={(e) => setInputTextValue(e.target.value)}
             />
-            {mode == NavbarMode.Add ? (
-                <button
-                    className="px-4 py-1 text-m w-32 font-semibold rounded border border-slate-200 text-white bg-slate-600 hover:bg-slate-500 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-slate-600 focus:ring-offset-2"
-                    onClick={addTack}
-                >
-                    add
-                </button>
-            ) : (
-                <></>
-            )}
 
-            {mode == NavbarMode.Search ? (
-                <button
-                    className="px-4 py-1 text-m w-32 font-semibold rounded border border-slate-200 text-white bg-slate-600 hover:bg-slate-500 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-slate-600 focus:ring-offset-2"
-                    onClick={search}
-                >
-                    search
-                </button>
+            <button
+                className="px-4 py-1 text-m w-32 font-semibold rounded border border-slate-200 text-white bg-slate-600 hover:bg-slate-500 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-slate-600 focus:ring-offset-2"
+                onClick={() => modeDetails[mode].btnOnClick()}
+            >
+                {modeDetails[mode].btnText}
+            </button>
+        </div>
+    );
+}
+
+function NavbarRight({
+    isLoggedIn,
+    user,
+    logout,
+    goToProfile,
+}: {
+    logout: Function;
+    goToProfile: Function;
+    isLoggedIn: boolean;
+    user?: UserType | null;
+}) {
+    return (
+        <div className="flex-wrap">
+            {isLoggedIn ? (
+                <>
+                    {user ? (
+                        <a
+                            onClick={() => goToProfile()}
+                            className="hover:cursor-pointer block mt-4 lg:inline-block lg:mt-0 text-stale-200 hover:text-slate-400 mr-5"
+                        >
+                            {user?.username}
+                        </a>
+                    ) : (
+                        <></>
+                    )}
+                    <a
+                        onClick={() => logout()}
+                        className="hover:cursor-pointer block mt-4 lg:inline-block lg:mt-0 text-stale-200 hover:text-slate-400 mr-5"
+                    >
+                        Logout
+                    </a>
+                </>
             ) : (
-                <></>
+                <>
+                    {" "}
+                    <Link href="/login">
+                        <a className="block mt-4 lg:inline-block lg:mt-0 text-stale-200 hover:text-slate-400 mr-5">
+                            Login
+                        </a>
+                    </Link>
+                    <Link href="/signup">
+                        <a className="block mt-4 lg:inline-block lg:mt-0 text-stale-200 hover:text-slate-400 mr-5">
+                            Sign Up
+                        </a>
+                    </Link>
+                </>
             )}
         </div>
     );
@@ -109,50 +166,16 @@ export default function Navbar() {
 
     return (
         <nav className="py-2 px-10 flex items-center justify-between flex-wrap bg-slate-50">
-            <div className="flex items-center flex-shrink-0 text-white mr-10">
+            <div className="flex items-center text-white">
                 <span className="font-semibold text-xl tracking-tight text-slate-600">Tack</span>
             </div>
-
             {isLoggedIn ? <NavbarInput username={user?.username || ""} /> : <></>}
-
-            <div className="w-full block flex-grow lg:flex lg:justify-end lg:w-auto">
-                <div className="text-sm">
-                    {isLoggedIn ? (
-                        <>
-                            {user ? (
-                                <a
-                                    onClick={goToProfile}
-                                    className="hover:cursor-pointer block mt-4 lg:inline-block lg:mt-0 text-stale-200 hover:text-slate-400 mr-5"
-                                >
-                                    {user?.username}
-                                </a>
-                            ) : (
-                                <></>
-                            )}
-                            <a
-                                onClick={logout}
-                                className="hover:cursor-pointer block mt-4 lg:inline-block lg:mt-0 text-stale-200 hover:text-slate-400 mr-5"
-                            >
-                                Logout
-                            </a>
-                        </>
-                    ) : (
-                        <>
-                            {" "}
-                            <Link href="/login">
-                                <a className="block mt-4 lg:inline-block lg:mt-0 text-stale-200 hover:text-slate-400 mr-5">
-                                    Login
-                                </a>
-                            </Link>
-                            <Link href="/signup">
-                                <a className="block mt-4 lg:inline-block lg:mt-0 text-stale-200 hover:text-slate-400 mr-5">
-                                    Sign Up
-                                </a>
-                            </Link>
-                        </>
-                    )}
-                </div>
-            </div>
+            <NavbarRight
+                logout={logout}
+                goToProfile={goToProfile}
+                user={user}
+                isLoggedIn={isLoggedIn}
+            />
         </nav>
     );
 }
