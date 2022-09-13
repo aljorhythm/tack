8b5f8c382a7ad1ce004eb9b8170d9bcc45aec6ea
Joel Lim <joel.lim@joels-mbp.lan>
2022-09-13 22:11:49 +0800

    unit test: add act statement for testing react component

--- components/navbar-input.test.tsx
+++ components/navbar-input.test.tsx
@@ -1,10 +1,12 @@
-import { fireEvent, render } from "@testing-library/react";
+import { act, fireEvent, render } from "@testing-library/react";
 import NavbarInput from "./navbar-input";
 
 test("input should be focused after selecting mode", async () => {
     const rendered = render(<NavbarInput username="" />);
     const editButton = rendered.container.querySelector(".set-mode-icon");
-    fireEvent.click(editButton!);
+    await act(() => {
+        fireEvent.click(editButton!);
+    });
     const input = await rendered.findByPlaceholderText("Search");
     expect(input).toHaveFocus();
 });
@@ -12,7 +14,9 @@ test("input should be focused after selecting mode", async () => {
 test("input should be cleared after selecting mode", async () => {
     const rendered = render(<NavbarInput username="" />);
     const input = await rendered.container.querySelector("input");
-    fireEvent.change(input!, { target: { value: "abc def" } });
+    await act(() => {
+        fireEvent.change(input!, { target: { value: "abc def" } });
+    });
     const editButton = rendered.container.querySelector(".set-mode-icon");
     fireEvent.click(editButton!);
     expect(input).toHaveValue("");
